55dc6090e338f0446cf1b80f34a6af4203d7ac8d
natthapol.n <natthapol.n@bossup.co.th>
2026-03-12 13:29:40 +0700

    Fix: update basepath

--- services/api.ts
+++ services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || '';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
 // Helper to get headers with current token
 const getHeaders = () => ({
